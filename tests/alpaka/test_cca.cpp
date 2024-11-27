967139aca0f6691b310871f5f4b499f1cb03a55e
Stewart Martin-Haugh <smh@cern.ch>
2024-11-27 17:39:23 +0100

    mess up formatting

--- tests/alpaka/test_cca.cpp
+++ tests/alpaka/test_cca.cpp
@@ -19,75 +19,75 @@
 
 namespace {
 
-// template <TAccTag>
-cca_function_t get_f_with(traccc::clustering_config cfg) {
+  // template <TAccTag>
+  cca_function_t get_f_with(traccc::clustering_config cfg) {
     return [cfg](const traccc::edm::silicon_cell_collection::host& cells,
-                 const traccc::silicon_detector_description::host& dd) {
-        std::map<traccc::geometry_id, vecmem::vector<traccc::measurement>>
-            result;
+        const traccc::silicon_detector_description::host& dd) {
+      std::map<traccc::geometry_id, vecmem::vector<traccc::measurement>>
+        result;
 
-        using namespace alpaka;
-        using Dim = DimInt<1>;
-        using Idx = uint32_t;
+      using namespace alpaka;
+      using Dim = DimInt<1>;
+      using Idx = uint32_t;
 
-        using Acc = ExampleDefaultAcc<Dim, Idx>;
-        traccc::alpaka::vecmem::host_device_types<
-            alpaka::trait::AccToTag<Acc>::type>::host_memory_resource host_mr;
-        traccc::alpaka::vecmem::host_device_types<
-            alpaka::trait::AccToTag<Acc>::type>::device_copy copy;
-        traccc::alpaka::vecmem::host_device_types<
-            alpaka::trait::AccToTag<Acc>::type>::device_memory_resource
-            device_mr;
+      using Acc = ExampleDefaultAcc<Dim, Idx>;
+      traccc::alpaka::vecmem::host_device_types<
+        alpaka::trait::AccToTag<Acc>::type>::host_memory_resource host_mr;
+      traccc::alpaka::vecmem::host_device_types<
+        alpaka::trait::AccToTag<Acc>::type>::device_copy copy;
+      traccc::alpaka::vecmem::host_device_types<
+        alpaka::trait::AccToTag<Acc>::type>::device_memory_resource
+        device_mr;
 
-        traccc::alpaka::clusterization_algorithm cc({device_mr}, copy, cfg);
+      traccc::alpaka::clusterization_algorithm cc({device_mr}, copy, cfg);
 
-        traccc::silicon_detector_description::buffer dd_buffer{
-            static_cast<
-                traccc::silicon_detector_description::buffer::size_type>(
-                dd.size()),
-            device_mr};
-        copy.setup(dd_buffer)->ignore();
-        copy(vecmem::get_data(dd), dd_buffer,
-             vecmem::copy::type::host_to_device)
-            ->ignore();
+      traccc::silicon_detector_description::buffer dd_buffer{
+        static_cast<
+          traccc::silicon_detector_description::buffer::size_type>(
+              dd.size()),
+          device_mr};
+      copy.setup(dd_buffer)->ignore();
+      copy(vecmem::get_data(dd), dd_buffer,
+          vecmem::copy::type::host_to_device)
+        ->ignore();
 
-        traccc::edm::silicon_cell_collection::buffer cells_buffer{
-            static_cast<
-                traccc::edm::silicon_cell_collection::buffer::size_type>(
-                cells.size()),
-            device_mr};
-        copy.setup(cells_buffer)->wait();
-        copy(vecmem::get_data(cells), cells_buffer)->wait();
+      traccc::edm::silicon_cell_collection::buffer cells_buffer{
+        static_cast<
+          traccc::edm::silicon_cell_collection::buffer::size_type>(
+              cells.size()),
+          device_mr};
+      copy.setup(cells_buffer)->wait();
+      copy(vecmem::get_data(cells), cells_buffer)->wait();
 
-        auto measurements_buffer = cc(cells_buffer, dd_buffer);
-        traccc::measurement_collection_types::host measurements{&host_mr};
-        copy(measurements_buffer, measurements)->wait();
+      auto measurements_buffer = cc(cells_buffer, dd_buffer);
+      traccc::measurement_collection_types::host measurements{&host_mr};
+      copy(measurements_buffer, measurements)->wait();
 
-        for (std::size_t i = 0; i < measurements.size(); i++) {
-            result[measurements.at(i).surface_link.value()].push_back(
-                measurements.at(i));
-        }
+      for (std::size_t i = 0; i < measurements.size(); i++) {
+        result[measurements.at(i).surface_link.value()].push_back(
+            measurements.at(i));
+      }
 
-        return result;
+      return result;
     };
-}
+  }
 }  // namespace
 
 TEST_P(ConnectedComponentAnalysisTests, Run) {
-    test_connected_component_analysis(GetParam());
+  test_connected_component_analysis(GetParam());
 }
 
 INSTANTIATE_TEST_SUITE_P(
     AlpakaFastSvAlgorithm, ConnectedComponentAnalysisTests,
     ::testing::Combine(
-        ::testing::Values(get_f_with(default_ccl_test_config())),
-        ::testing::ValuesIn(ConnectedComponentAnalysisTests::get_test_files())),
+      ::testing::Values(get_f_with(default_ccl_test_config())),
+      ::testing::ValuesIn(ConnectedComponentAnalysisTests::get_test_files())),
     ConnectedComponentAnalysisTests::get_test_name);
 
 INSTANTIATE_TEST_SUITE_P(
     AlpakaFastSvAlgorithmWithScratch, ConnectedComponentAnalysisTests,
     ::testing::Combine(
-        ::testing::Values(get_f_with(tiny_ccl_test_config())),
-        ::testing::ValuesIn(
-            ConnectedComponentAnalysisTests::get_test_files_short())),
+      ::testing::Values(get_f_with(tiny_ccl_test_config())),
+      ::testing::ValuesIn(
+        ConnectedComponentAnalysisTests::get_test_files_short())),
     ConnectedComponentAnalysisTests::get_test_name);
